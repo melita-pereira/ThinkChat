65c239cbeeb6b1cb8922a2fbfcb56c337b2479c8
melita-pereira <melita_pereira@yahoo.com>
2023-06-25 18:23:47 +0400

    Updated files to set up Firebase Cloud Messaging and Firebase Database

--- app/src/main/java/com/melita/thinkchat/activities/SignInActivity.java
+++ app/src/main/java/com/melita/thinkchat/activities/SignInActivity.java
@@ -4,24 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.melita.thinkchat.R;
-import com.melita.thinkchat.databinding.ActivitySignInBinding;
 
-public class SignInActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
-    private ActivitySignInBinding binding;
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    
+    Button buttonSignIn;
+    TextView textCreateNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignInBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        setListeners();
+        setContentView(R.layout.activity_sign_in);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(this);
+        textCreateNewAccount = findViewById(R.id.textCreateNewAccount);
+        textCreateNewAccount.setOnClickListener(this);
     }
 
-    private void setListeners(){
-        binding.textCreateNewAccount.setOnClickListener(v ->
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.buttonSignIn){
+            addDataToFirestore();
+        } else if (id == R.id.textCreateNewAccount) {
+            startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        }
+    }
+
+    private void addDataToFirestore(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> data = new HashMap<>();
+        data.put("first_name", "Melita");
+        data.put("last_name", "Pereira");
+        db.collection("users")
+                .add(data)
+                .addOnSuccessListener(aVoid -> Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 }
