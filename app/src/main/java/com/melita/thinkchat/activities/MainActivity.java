e7ddc51614961bd20755719059cbb48bba09f622
melita-pereira <melita_pereira@yahoo.com>
2023-09-17 01:18:14 +0400

    Updated files to set up Sign Up Process

---
+++ app/src/main/java/com/melita/thinkchat/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.melita.thinkchat.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.melita.thinkchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
